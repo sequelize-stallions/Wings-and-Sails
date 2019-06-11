498a0f382c3cb80f51ffe1f70973b4e88e8ca91a
Maria Obaturova <m.obaturova@gmail.com>
2019-06-11 16:13:53 -0400

    fixed Noah`s comment

--- client/routes.js
+++ client/routes.js
@@ -20,7 +20,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route exact path="/products" render={() => <AllProducts />} />
+        <Route exact path="/products" component={AllProducts} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn && (
