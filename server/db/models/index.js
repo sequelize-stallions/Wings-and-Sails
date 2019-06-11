0910142a6509daf5bac939e81991309c6f054933
daphne178 <realta23dc@gmail.com>
2019-06-11 15:32:27 -0400

    fixed assosiation

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -18,8 +18,7 @@ const Cart = require('./cart')
 
 User.hasMany(Cart)
 Cart.belongsTo(User)
-Cart.hasMany(Product)
-Product.belongsTo(Cart)
+Product.belongsToMany(Cart)
 
 module.exports = {
   User,
