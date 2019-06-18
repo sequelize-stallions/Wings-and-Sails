84a823ddba6c3853139562d312dbd0022bad14e5
daphne178 <realta23dc@gmail.com>
2019-06-18 18:35:22 -0400

    fix(admin): removed extra code from users express route

--- server/api/users.js
+++ server/api/users.js
@@ -2,20 +2,6 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-// router.get('/', async (req, res, next) => {
-//   try {
-//     const users = await User.findAll({
-//       // explicitly select only the id and email fields - even though
-//       // users' passwords are encrypted, it won't help if we just
-//       // send everything to anyone who asks!
-//       attributes: ['id', 'email']
-//     })
-//     res.json(users)
-//   } catch (err) {
-//     next(err)
-//   }
-// })
-
 //Getting all users
 router.get('/', async (req, res, next) => {
   try {
@@ -28,10 +14,6 @@ router.get('/', async (req, res, next) => {
         'lastName',
         'address',
         'email',
-        'password',
-        'salt',
-        'googleId',
-        'admin',
         'fullName'
       ]
     })
