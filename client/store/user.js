19b0774125cf838a4743b8a6d47bea90470f71d6
daphne178 <realta23dc@gmail.com>
2019-06-18 18:43:11 -0400

    fix(admin): removed unused code from user reducer

--- client/store/user.js
+++ client/store/user.js
@@ -5,7 +5,6 @@ import history from '../history'
  * ACTION TYPES
  */
 const GET_USER = 'GET_USER'
-const GET_USERS = 'GET_USERS'
 const REMOVE_USER = 'REMOVE_USER'
 
 /**
@@ -13,16 +12,10 @@ const REMOVE_USER = 'REMOVE_USER'
  */
 const defaultUser = {}
 
-const initialUsers = {
-  users: [],
-  defaultUser: {}
-}
-
 /**
  * ACTION CREATORS
  */
 const getUser = user => ({type: GET_USER, user})
-const getAllUsers = users => ({type: GET_USERS, users})
 const removeUser = () => ({type: REMOVE_USER})
 
 /**
@@ -63,32 +56,16 @@ export const logout = () => async dispatch => {
   }
 }
 
-export const thunkGetUsers = () => {
-  return async dispatch => {
-    try {
-      const {data} = await axios.get('/api/users')
-      dispatch(getAllUsers(data))
-    } catch (err) {
-      console.log('Thunk get users!', err)
-    }
-  }
-}
-
 /**
  * REDUCER
  */
 
-export default function(state = initialUsers, action) {
+export default function(state = defaultUser, action) {
   switch (action.type) {
     case GET_USER:
       return action.user
     case REMOVE_USER:
       return defaultUser
-    case GET_USERS:
-      return {
-        ...state,
-        users: action.users
-      }
     default:
       return state
   }
